7e2064964a335ef3a603030776daf99783c28bb1
Abhishek <241abhishek@gmail.com>
2023-11-17 13:03:06 -0600

    Robust guess verification for letters and words.

--- ocr/ocr/ocr.py
+++ ocr/ocr/ocr.py
@@ -16,13 +16,15 @@ class Ocr(Node):
     def __init__(self):
         super().__init__("ocr")
 
+        # initialize paddleocr class
         self.ocr = PaddleOCR(lang='en', use_gpu= False) # need to run only once to download and load model into memory
-        
-        self.guess_publish = self.create_publisher(String, "user_input", 10)
+        # initialize CvBridge
         self.cv_bridge = CvBridge()
         
-        # self.cap = cv2.VideoCapture(0)
+        # create publisher to publish guesses
+        self.guess_publish = self.create_publisher(String, "user_input", 10)
         
+        # create subscriber to get image
         self.cap = self.create_subscription(Image, "camera/color/image_raw", self.image_modification, qos_profile=10)
         
         #declare and define parameters
@@ -30,18 +32,21 @@ class Ocr(Node):
         self.param_ocr_frequency = self.get_parameter('ocr_frequency').get_parameter_value().integer_value
         self.declare_parameter('ocr_threshold', 0.85)
         self.param_ocr_threshold = self.get_parameter('ocr_threshold').get_parameter_value().double_value
-
-        self.count = 1
+        
+        # define instance attributes
         self.frame = None
-        self.guess_tracker = []
-        self.guess_pub_tracker = []
+        self.guess_tracker = [] # queue verified guesses
+        self.guess_pub_tracker = [] # track published guesses
 
+        # create timer for calling the ocr function
         self.timer = self.create_timer(1.0/self.param_ocr_frequency, self.ocr_timer)
 
     def ocr_timer(self):
+        """Call the ocr function"""
         self.ocr_func(self.frame)
 
     def ocr_func(self, frame):
+        """Run OCR on the image frame"""
         result = self.ocr.ocr(frame, cls=False)
         if result[0] != None:
             self.guess_verification(result)
@@ -49,12 +54,26 @@ class Ocr(Node):
         # self.get_logger().info(f"Result: {result}")
 
     def guess_verification(self, result):
-        if len(result[0][0][1][0]) == 1 or len(result[0][0][1][0]) == 6:
-            if result[0][0][1][1] > self.param_ocr_threshold:
-                self.guess_tracking(result[0][0][1][0])
-                # self.get_logger().info(f"Registering Guess: {result[0][0][1][0]}")
+        """Confirm whether the guess is a single letter or 6L word"""
+        try:
+            # check if the guess is a single letter according to instructions
+            if result[0][0][1][0][-2] == ':' and result[0][0][1][0][-1].isalpha(): # check for single characters
+                if result[0][0][1][1] > self.param_ocr_threshold: # check confidence
+                    # Use the split() method to split the string into 
+                    # letter = result[0][0][1][0].split(':')
+                    print(result[0][0][1][0][-1])
+                    self.guess_tracking(result[0][0][1][0][-1])
+            elif result[0][0][1][0][-2] == ':' and result[0][0][1][0][-1] == '0': # catch exception for "O"
+                if result[0][0][1][1] > self.param_ocr_threshold: # check confidence
+                    self.guess_tracking('O')
+            elif len(result[0][0][1][0]) == 6 and result[0][0][1][0].isalpha(): # check if guess is a 6L word with letters only
+                if result[0][0][1][1] > self.param_ocr_threshold: # check confidence
+                    self.guess_tracking(result[0][0][1][0])
+        except:
+            pass
 
     def guess_tracking(self, guess):
+        """Keep track of the previous guesses to confirm accuracy"""
         self.guess_tracker.append(guess)
         if len(self.guess_tracker) > 3:
             del self.guess_tracker[0]
@@ -65,8 +84,10 @@ class Ocr(Node):
                 self.guess_publisher(guess)
 
     def guess_publisher(self, guess):
+        """Publish the verified guess"""
         current_guess = String()
         publish = True
+        # check if the guess has already been published
         if len(self.guess_pub_tracker) > 0:
             for item in self.guess_pub_tracker:
                 if item == guess:
@@ -76,8 +97,8 @@ class Ocr(Node):
             current_guess.data = guess
             self.guess_publish.publish(current_guess)
 
-
     def image_modification(self, msg):
+        """Convert image to opencv format"""
         self.frame = self.cv_bridge.imgmsg_to_cv2(msg, "bgr8")
         cv2.imshow("image", self.frame)
         cv2.waitKey(1)
