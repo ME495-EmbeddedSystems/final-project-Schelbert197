bd77fedb1890e1a6a8e086bad4cb81b4716a5ea7
Abhishek <241abhishek@gmail.com>
2023-11-17 14:56:26 -0600

    Added the hangman game file to the OCR package.

--- ocr/setup.py
+++ ocr/setup.py
@@ -20,7 +20,8 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            "ocr = ocr.ocr:main"
+            "ocr = ocr.ocr:main",
+            "hangman = ocr.hangman:main"
         ],
     },
 )
