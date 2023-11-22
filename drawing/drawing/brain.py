43bafa2e7356b4698170808a1f3184f5a0f17a2e
Srikanth Schelbert <srikanthschelbert2024@u.northwestern.edu>
2023-11-22 12:24:54 -0600

    Heavily commented the code for readability. Many TODO comments though structure is there Made pose a list in custom srv

--- drawing/drawing/brain.py
+++ drawing/drawing/brain.py
@@ -15,8 +15,10 @@ from enum import Enum, auto
 
 
 class State(Enum):
-    INITIALIZE = auto()
-    WAITING = auto()
+    INITIALIZE = auto(),
+    CALIBRATE = auto(),
+    SETUP = auto(),
+    WAITING = auto(),
     LETTER = auto()
 
 
@@ -111,7 +113,9 @@ class Brain(Node):
     def hangman_callback(self, msg:LetterMsg):
         """Callback when feedback is given from hangman"""
 
+        # establishes a global message variable for the duration of the letter state
         self.last_message = msg
+        # switches to letter
         self.state = State.LETTER
 
     def timer_callback(self):
@@ -122,26 +126,52 @@ class Brain(Node):
 
             # its possible this message is sent too fast, and that draw.py
             # doesn't receive it, just keep in mind.
+            
+            # Moves to the tag calibration state once the robot has reached the home position
+            self.state = State.CALIBRATE
 
+        elif self.state == State.CALIBRATE:
+
+            self.calibrate_client() # TODO: we will need to add this client that calls the calibrate action
             # This should send the camera calibration service as well
             # TODO: Ananya can put this to have that service call how she prefers
 
+            # moves to setup state where we draw the dashes and stuff once the calibration is complete
+            self.state = State.SETUP
+
+        elif self.state == State.SETUP:
+
+            # TODO: Add the client/action for the robot to draw the noose and the dashes
+            self.setup_client()
+
+            # Moves to the waiting state once we are setup, and waits for something to happen from hangman.py
+            self.state = State.WAITING
+
         elif self.state == State.LETTER:
 
+            # This for loop will run for each thing that we need to draw based on the message sent from hangman
+            # TODO We will need to consider asynchronicity, but this is the flow of info
             for j in range(0,len(self.last_message.positions)):
+                # Ananya's code gives origin for the tile that we are working in reference to (taking the mode group and position in group)
                 self.tile_pose:Pose = self.board_service.call_async(self.last_message.mode, self.last_message.positions[j])
 
+                # Coords to poses takes in the letter and the pose from the board and returns a list of poses for the trajectory
                 letter_poses = self.coords_to_poses(self.last_message.letter, self.tile_pose)
                 start_point = Pose(
                     position=Point(x=self.tile_pose.position.x - 0.1, y=self.tile_pose.position.y, z=self.tile_pose.position.z),
                     orientation=self.tile_pose.orientation
                 ) # start point given by service call TODO: Ananya, please check if this is correct for the start pos
+                # I have made this start point to be 10cm behind the board facing the origin of the tile in question
 
+                # cartesian message type packages the start point and the list of letter poses
                 cartesian_msg = Cartesian(
                     poses=letter_poses, start_point=start_point)
 
                 # publish the message, draw.py is a subscriber
                 self.cartesian_mp_pub.publish(cartesian_msg)
 
+            # TODO: Will need to add the call to start the OCR before we go to the waiting state
+            self.state = State.WAITING
+            
         elif self.state == State.WAITING:
             pass
