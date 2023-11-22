b35ea71871b1b379d3818ea66aed79b7c8e9b73e
Srikanth Schelbert <srikanthschelbert2024@u.northwestern.edu>
2023-11-21 19:41:14 -0600

    Created brain.py fcns custom srvc for board tiles also created functions for brain.py to communicate with other node. work in progress

--- drawing/drawing/brain.py
+++ drawing/drawing/brain.py
@@ -3,9 +3,12 @@ from rclpy.node import Node
 from rclpy.callback_groups import MutuallyExclusiveCallbackGroup
 
 from std_srvs.srv import Empty
-
+from matplotlib.font_manager import FontProperties
+from matplotlib.textpath import TextToPath
 from brain_interfaces.msg import Cartesian
-from character_interfaces.alphabet import alphabet
+from brain_interfaces.srv import BoardTiles
+from gameplay_interfaces.msg import LetterMsg
+# from character_interfaces.alphabet import alphabet
 from geometry_msgs.msg import Pose, Point, Quaternion
 
 from enum import Enum, auto
@@ -38,6 +41,14 @@ class Brain(Node):
 
         self.test_service = self.create_service(
             Empty, '/test_brain', self.test_service_callback)
+        self.board_service = self.create_client(
+            BoardTiles, '/board_tiles') # create custom service type
+        self.ocr_service = self.create_service(
+            Empty, '/ocr_service', self.test_service_callback)
+        
+        # Create subscription from hangman.py
+        self.hangman = self.create_subscription(
+            LetterMsg, '/writer', callback=self.hangman_callback, qos_profile=10)
 
         # define global variables
 
@@ -45,19 +56,63 @@ class Brain(Node):
             position=Point(x=-0.5, y=0.0, z=0.4),
             orientation=Quaternion(x=1.0, y=0.0, z=0.0, w=0.0)
         )
+        self.alphabet = {}
+        self.scale_factor = 0.01
 
         self.state = State.INITIALIZE
 
-    def coords_to_poses(self, char):
-        # get the coordiantes for the letter from the dictionary
-        coords = alphabet(char)
-        # TODO: write a for loop to create a list of Pose() from the coords
+    def create_letters(self):
+        """Create the dictionary of bubble letters"""
 
+        letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        for i in range(0,len(letters)):
+            letter = letters[i]
+            fp = FontProperties(family="MS Gothic", style="normal")
+            verts, codes = TextToPath().get_text_path(fp, letters[i])
+            xlist = []
+            ylist = []
+            for j in range(0,len(verts) - 1):
+                # if verts[j][0] > 0: Commented out because I want to keep the 0,0 for lifting off the board
+                xlist.append(verts[j][0])
+                ylist.append(verts[j][1])
+            point_dict = {letter: {'xlist': xlist,'ylist':ylist}}
+            self.alphabet.update(point_dict)
+    
+    def coords_to_poses(self, letter, tilepose:Pose):
+        # get the coordiantes for the letter from the dictionary
+        xcoord = self.alphabet[letter]['xlist']
+        ycoord = self.alphabet[letter]['ylist']
+        poses = []
+        # TODO: write a for loop to create a list of Pose() from the coords
+        for i in range(0,len(xcoord)):
+            if xcoord[i] > 0 and ycoord[i] > 0:
+                p = Point(x=tilepose.position.x,
+                        y=tilepose.position.y + (xcoord[i] * self.scale_factor),
+                        z=tilepose.position.z + (ycoord[i] * self.scale_factor))
+                quat = tilepose.orientation
+                point_pose = Pose(position=p, orientation=quat)
+            else:
+                p = Point(x=tilepose.position.x - 0.2,
+                        y=tilepose.position.y + (xcoord[i] * self.scale_factor),
+                        z=tilepose.position.z + (ycoord[i] * self.scale_factor))
+                quat = tilepose.orientation
+                point_pose = Pose(position=p, orientation=quat)
+            poses.append(point_pose)
+                
     def test_service_callback(self, request, response):
 
         self.state = State.LETTER
 
         return response
+    
+    # def board_service_callback(self, request, response):
+    #     """Callback for the service to get the board tile pose"""
+    
+    def hangman_callback(self, msg:LetterMsg):
+        """Callback when feedback is given from hangman"""
+
+        self.last_message = msg
+        self.state = State.LETTER
 
     def timer_callback(self):
         if self.state == State.INITIALIZE:
@@ -68,22 +123,25 @@ class Brain(Node):
             # its possible this message is sent too fast, and that draw.py
             # doesn't receive it, just keep in mind.
 
+            # This should send the camera calibration service as well
+            # TODO: Ananya can put this to have that service call how she prefers
+
         elif self.state == State.LETTER:
-            # TODO: write a function coords_to_poses() to transform the list of
-            # y and z coordinates we have for writing letters into a list of poses
-            # that we can send on the /cartesian_mp topic
 
-            letter_poses = []  # instead of empty list, = coords_to_poses()
-            start_point = Pose(
-                position=Point(x=0.0, y=0.0, z=0.4),
-                orientation=Quaternion(x=1.0, y=0.0, z=0.0, w=0.0)
-            )
+            for j in range(0,len(self.last_message.positions)):
+                self.tile_pose:Pose = self.board_service.call_async(self.last_message.mode, self.last_message.positions[j])
 
-            cartesian_msg = Cartesian(
-                poses=letter_poses, start_point=start_point)
+                letter_poses = self.coords_to_poses(self.last_message.letter, self.tile_pose)
+                start_point = Pose(
+                    position=Point(x=self.tile_pose.position.x - 0.1, y=self.tile_pose.position.y, z=self.tile_pose.position.z),
+                    orientation=self.tile_pose.orientation
+                ) # start point given by service call TODO: Ananya, please check if this is correct for the start pos
 
-            # publish the message, draw.py is a subscriber
-            self.cartesian_mp_pub.publish(cartesian_msg)
+                cartesian_msg = Cartesian(
+                    poses=letter_poses, start_point=start_point)
+
+                # publish the message, draw.py is a subscriber
+                self.cartesian_mp_pub.publish(cartesian_msg)
 
         elif self.state == State.WAITING:
             pass
