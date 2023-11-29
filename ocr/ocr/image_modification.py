c60bdc30bd17e5f6aa4141cd495ddda52f88d033
Abhishek <241abhishek@gmail.com>
2023-11-29 17:40:02 -0600

    Invert and dilate modified image.

--- ocr/ocr/image_modification.py
+++ ocr/ocr/image_modification.py
@@ -1,5 +1,6 @@
 import rclpy
 from rclpy.node import Node
+import numpy as np
 
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
@@ -27,13 +28,19 @@ class ImageModification(Node):
         gray = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
         # cv2.imshow("gray", gray)
 
-        blurred = cv2.GaussianBlur(gray, (11, 11), 0)
+        # blurred = cv2.GaussianBlur(gray, (11, 11), 0)
         # cv2.imshow("blurred", blurred)
 
-        ret3,th3 = cv2.threshold(blurred,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-        cv2.imshow("binarized", th3)
+        # ret3,th3 = cv2.threshold(blurred,127,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+        ret3,th3 = cv2.threshold(gray,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
 
-        img_publish = self.cv_bridge.cv2_to_imgmsg(th3)
+        # cv2.imshow("binarized", th3)
+        kernel = np.ones((17,17),np.uint8)
+        dilation = cv2.dilate(th3,kernel,iterations = 1)
+        inverted_image = cv2.bitwise_not(dilation)
+        cv2.imshow("inverted_image", inverted_image)
+
+        img_publish = self.cv_bridge.cv2_to_imgmsg(inverted_image)
         self.modified_image_publish.publish(img_publish)
         cv2.waitKey(1)
 
