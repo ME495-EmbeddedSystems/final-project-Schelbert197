059e049c4c79fc9f0cf5ee128e62114e6edb8209
Abhishek <241abhishek@gmail.com>
2023-11-30 13:24:38 -0600

    Added contour detection and 4-pt-transform.
    This isolates the whiteboard in the image.

--- ocr/ocr/image_modification.py
+++ ocr/ocr/image_modification.py
@@ -2,6 +2,7 @@ import rclpy
 from rclpy.node import Node
 import numpy as np
 import imutils
+from imutils.perspective import four_point_transform
 
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
@@ -43,11 +44,40 @@ class ImageModification(Node):
         blurred = cv2.GaussianBlur(gray, (11, 11), 0)
         cv2.imshow("blurred", blurred)
         ret3,binarised = cv2.threshold(gray,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
-        # cv2.imshow("binarized", binarised)
+        cv2.imshow("binarised", binarised)
         c_min = cv2.getTrackbarPos('Canny_T_min', 'Parameters')
         c_max = cv2.getTrackbarPos('Canny_T_max', 'Parameters')
         edged = cv2.Canny(blurred, c_min, c_max)
         cv2.imshow("edged", edged)
+        # edged_2 = cv2.Canny(gray, c_min, c_max)
+        # cv2.imshow("edged_2", edged_2)
+
+        # find contours in the edge map, then sort them by their
+        # size in descending order
+        cnts = cv2.findContours(edged.copy(), cv2.RETR_TREE,
+            cv2.CHAIN_APPROX_SIMPLE)
+        cnts = imutils.grab_contours(cnts)
+        cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
+        displayCnt = None
+
+        # loop over the contours
+        for c in cnts:
+            # approximate the contour
+            peri = cv2.arcLength(c, True)
+            approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+            # if the contour has four vertices, then we have found
+            # the whiteboard
+            if len(approx) == 4:
+                displayCnt = approx
+                break
+        # extract the bounded whiteboard region, apply a perspective transform
+        # to it
+        try:
+            warped = four_point_transform(gray, displayCnt.reshape(4, 2))
+            output = four_point_transform(resized_image, displayCnt.reshape(4, 2))
+            cv2.imshow("transformed", warped)
+        except:
+            pass
 
         # kernel = np.ones((13,13),np.uint8)
         # dilation = cv2.dilate(binarised,kernel,iterations = 1)
