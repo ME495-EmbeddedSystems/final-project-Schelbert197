91c8f46cb63d8becf203a5124ac88ba9d58bbd2b
Srikanth Schelbert <srikanthschelbert2024@u.northwestern.edu>
2023-12-05 18:05:08 -0600

    flake 8 errors removed. Pep257 unfinished

--- drawing/drawing/brain.py
+++ drawing/drawing/brain.py
@@ -1,15 +1,25 @@
+"""
+Plays the hangman game based on the OCR user input.
+
+Interfaces with the brain node and the OCR node to evaulate data.
+PUBLISHERS:
+  + /writer (LetterMsg) - The data sent to brain for a given play.
+SERVICES:
+    none
+PARAMETERS:
+    none
+"""
+
 import rclpy
 from rclpy.node import Node
 from rclpy.callback_groups import MutuallyExclusiveCallbackGroup
 
 from std_srvs.srv import Empty
-from std_msgs.msg import Bool, String
+from std_msgs.msg import Bool
 from matplotlib.font_manager import FontProperties
 from matplotlib.textpath import TextToPath
-# from brain_interfaces.msg import Cartesian
-from brain_interfaces.srv import BoardTiles, MovePose, Cartesian, Box
+from brain_interfaces.srv import BoardTiles, MovePose, Cartesian
 from brain_interfaces.msg import LetterMsg
-# from character_interfaces.alphabet import alphabet
 from geometry_msgs.msg import Pose, Point, Quaternion
 
 from enum import Enum, auto
@@ -18,10 +28,7 @@ import numpy as np
 
 class State(Enum):
     INITIALIZE = auto(),
-    CALIBRATE = auto(),
-    APPROACHING = auto(),
     WAITING = auto(),
-    WRITING = auto(),
     LETTER = auto()
 
 
@@ -38,55 +45,40 @@ class Brain(Node):
         self.moveit_mp_pub = self.create_publisher(
             Pose, '/moveit_mp', 10)
 
-        self.state_pub = self.create_publisher(
-            String, '/brain_states', 10)  # maybe use this to publish the states
-
         self.ocr_pub = self.create_publisher(
             Bool, '/ocr_run', 10)
 
         # Callback groups
-        self.cal_callback_group = MutuallyExclusiveCallbackGroup()
-        self.tile_callback_group = MutuallyExclusiveCallbackGroup()
+        self.tile_cb_group = MutuallyExclusiveCallbackGroup()
         self.mp_callback_group = MutuallyExclusiveCallbackGroup()
-        self.cartesian_callback_group = MutuallyExclusiveCallbackGroup()
-        self.kick_callback_group = MutuallyExclusiveCallbackGroup()
-        self.make_board_callback_group = MutuallyExclusiveCallbackGroup()
+        self.cartesian_cb_group = MutuallyExclusiveCallbackGroup()
+        self.kick_cb_group = MutuallyExclusiveCallbackGroup()
 
         # Create clients
         self.board_service_client = self.create_client(
-            BoardTiles, '/where_to_write', callback_group=self.tile_callback_group)  # create custom service type
-        self.calibrate_service_client = self.create_client(
-            Empty, 'calibrate', callback_group=self.cal_callback_group)  # create custom service type
-        self.movepose_service_client = self.create_client(
-            MovePose, '/moveit_mp', callback_group=self.mp_callback_group)  # create custom service type
-        self.cartesian_mp_service_client = self.create_client(
-            Cartesian, '/cartesian_mp', callback_group=self.cartesian_callback_group)  # create custom service type
-        self.kickstart_service_client = self.create_client(
-            Empty, '/kickstart_service', callback_group=self.kick_callback_group)
-        # self.make_board_client = self.create_client(
-        #     Box, '/make_board', callback_group=self.make_board_callback_group)
+            BoardTiles, '/where_to_write', callback_group=self.tile_cb_group)
+        self.movepose_client = self.create_client(
+            MovePose, '/moveit_mp', callback_group=self.mp_callback_group)
+        self.cartesian_mp_client = self.create_client(
+            Cartesian, '/cartesian_mp', callback_group=self.cartesian_cb_group)
+        self.kickstart_client = self.create_client(
+            Empty, '/kickstart_service', callback_group=self.kick_cb_group)
 
-        while not self.calibrate_service_client.wait_for_service(timeout_sec=1.0):
-            self.get_logger().info('Calibrate service not available, waiting...')
         while not self.board_service_client.wait_for_service(timeout_sec=1.0):
-            self.get_logger().info('Where to Write service not available, waiting...')
-        while not self.movepose_service_client.wait_for_service(timeout_sec=1.0):
-            self.get_logger().info('Move It MP service not available, waiting...')
-        while not self.cartesian_mp_service_client.wait_for_service(timeout_sec=1.0):
-            self.get_logger().info('Carisiam mp  service not available, waiting...')
-        while not self.kickstart_service_client.wait_for_service(timeout_sec=1.0):
-            self.get_logger().info('Kickstart  service not available, waiting...')
+            self.get_logger().info('Board service not available, waiting...')
+        while not self.movepose_client.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('MoveIt MP service unavailable, waiting...')
+        while not self.cartesian_mp_client.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('Carisian service unavailable, waiting...')
+        while not self.kickstart_client.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('Kickstart service unavailable, waiting...')
 
         # Create subscription from hangman.py
         self.hangman = self.create_subscription(
-            LetterMsg, '/writer', callback=self.hangman_callback, qos_profile=10)
-        # self.home = self.create_subscription(
-        #     Bool, '/RTH', callback=self.home_callback, qos_profile=10)
-        # self.trajectory_status = self.create_subscription(
-        #     String, '/execute_trajectory_status',callback=self.trajectory_status_callback, qos_profile=10)
+            LetterMsg, '/writer',
+            callback=self.hangman_callback, qos_profile=10)
 
         # define global variables
-
         self.home_position = Pose(
             position=Point(x=-0.5, y=0.0, z=0.4),
             orientation=Quaternion(x=1.0, y=0.0, z=0.0, w=0.0)
@@ -150,12 +142,13 @@ class Brain(Node):
                 point_dict = {letter: {'xlist': xlist, 'ylist': ylist}}
                 self.alphabet.update(point_dict)
             else:  # All letters of alphabet
-                fp = FontProperties(family="Liberation Sans Narrow", style="normal")
+                fp = FontProperties(
+                    family="Liberation Sans Narrow", style="normal")
                 verts, codes = TextToPath().get_text_path(fp, letters[i])
                 xlist = []
                 ylist = []
                 for j in range(0, len(verts) - 1):
-                    if verts[j][0] > 0: #Commented out because I want to keep the 0,0 for lifting off the board
+                    if verts[j][0] > 0:
                         xlist.append(
                             verts[j][0] * self.scale_factor * self.board_scale)
                         ylist.append(
@@ -164,14 +157,30 @@ class Brain(Node):
                 self.alphabet.update(point_dict)
 
     def process_letter_points(self, letter):
-        """ Function to make it easier to prepare letters for board tile type"""
+        """
+        Prepare the letter points
+
+        Function to make it easier to prepare letters for board tile type
+
+        Args:
+        ----
+        letter (String) : The character to be written on the board
+
+        Returns:
+        -------
+        board_x (List) : The list of x points on the board plane
+        board_y (List) : The list of y points on the board plane
+        board_bool (List) : The list of boolean values on the board
+
+        """
         xcoord = self.alphabet[letter]['xlist']
         ycoord = self.alphabet[letter]['ylist']
         board_x = []
         board_y = []
         board_bool = []
         for i in range(0, len(xcoord)):
-            if not (0.0001 > xcoord[i] > -0.0001) or not (0.0001 > ycoord[i] > 0.0001):
+            if not (0.0001 > xcoord[i] > -0.0001) \
+                    or not (0.0001 > ycoord[i] > 0.0001):
                 board_x.append(xcoord[i])
                 board_y.append(ycoord[i])
                 board_bool.append(True)
@@ -185,21 +194,10 @@ class Brain(Node):
                 board_bool.append(False)
         return board_x, board_y, board_bool
 
-    # def trajectory_status_callback(self, msg: String):
-    #     """Callback for the service to get execute the drawing on the board"""
-    #     new_msg = msg
-    #     if new_msg == 'done':
-    #         # Remove the first instance in the shape list since it was just executed
-    #         self.shape_list.pop(0)
-    #         # Return to letter writing to see if more things need to be written
-    #         self.state = State.LETTER
-    #     # else:
-    #         # self.get_logger().error("An error occured and the trajectory did not return done.")
-
     def hangman_callback(self, msg: LetterMsg):
         """Callback when feedback is given from hangman"""
 
-        # establishes a global message variable for the duration of the letter state
+        # establishes a global message variable for the duration of LETTER
         self.last_message = msg
         self.ocr_pub.publish(Bool(data=False))
 
@@ -213,20 +211,12 @@ class Brain(Node):
             tile_origin.position = self.last_message.positions[i]
 
             # get x, y, onboard values
-            tile_origin.x, tile_origin.y, tile_origin.onboard = self.process_letter_points(
-                self.last_message.letters[i])
+            tile_origin.x, tile_origin.y, tile_origin.onboard = \
+                self.process_letter_points(self.last_message.letters[i])
             self.shape_list.append(tile_origin)
 
         # switches to calibrate state
-        self.state = State.CALIBRATE
-
-    def home_callback(self, msg: Bool):
-        """Callback for whether or not the robot has returned to home after writing"""
-        if msg == True:
-            self.ocr_pub.publish(Bool(data=True))
-            self.state = State.WRITING
-        else:
-            self.state = State.WAITING
+        self.state = State.LETTER
 
     async def letter_writer(self, shape: BoardTiles.Request()):
         """Function to process the shape into trajectory service calls"""
@@ -234,126 +224,77 @@ class Brain(Node):
         pose1 = resp.initial_pose
         pose_list = resp.pose_list
 
-        self.get_logger().info(f"Pose List for Dash: {pose1}")
-        self.get_logger().info(f"Pose List for Dash: {pose_list}")
-
         request2 = MovePose.Request()
         request2.target_pose = pose1
         request2.use_force_control = False
-        await self.movepose_service_client.call_async(request2)
-        self.get_logger().info(f"one done")
+        await self.movepose_client.call_async(request2)
 
         request2 = Cartesian.Request()
         request2.poses = [pose_list[0]]
         request2.velocity = 0.015
         request2.replan = False
         request2.use_force_control = [shape.onboard[0]]
-        await self.cartesian_mp_service_client.call_async(request2)
-        self.get_logger().info(f"second done")
-        # draw remaining pose dashes with Cartesian mp
+        await self.cartesian_mp_client.call_async(request2)
+
         request3 = Cartesian.Request()
         request3.poses = pose_list[1:]
         request3.velocity = 0.015
         request3.replan = True
         request3.use_force_control = shape.onboard[1:]
-        self.get_logger().info(f"pose_list: {pose_list[1:]}")
-        await self.cartesian_mp_service_client.call_async(request3)
-        self.get_logger().info(f"all done")
+        await self.cartesian_mp_client.call_async(request3)
 
         self.shape_list.pop(0)
 
     async def timer_callback(self):
-
+        """Timer running at a specified frequency"""
         if self.state == State.INITIALIZE:
-
             # Initializes the kickstart feature then waits for completion
-            await self.kickstart_service_client.call_async(request=Empty.Request())
-            # Turns on the OCR because the play has ended and returns to WAITING
+            await self.kickstart_client.call_async(request=Empty.Request())
             goal_js = MovePose.Request()
-            # goal_js.joint_names = ["panda_joint4", "panda_joint5", "panda_joint7"]
-            # goal_js.joint_positions = [-2.61799, -1.04173, 2.11185]
             goal_js.target_pose.position = Point(
-                x=0.545029890155533, y=0.05943234468738731, z=0.5893544164237723)
-            goal_js.target_pose.orientation = Quaternion(
-                x=-0.48576480709767544, y=-0.5175973920275, z=-0.4696623291331898, w=0.5249216975367619)
+                x=0.545029890155533, y=0.05943234468738731, z=0.58935441642377)
+            goal_js.target_pose.orientation = Quaternion(x=-0.4857648070976754,
+                                                         y=-0.5175973920275,
+                                                         z=-0.4696623291331898,
+                                                         w=0.5249216975367619)
             goal_js.use_force_control = False
-            ##################### moving to the position####################
             self.get_logger().info('before moved')
-            await self.movepose_service_client.call_async(goal_js)
+            await self.movepose_client.call_async(goal_js)
+            # Turns on OCR because the play has ended and returns to WAITING
             self.ocr_pub.publish(Bool(data=True))
             self.state = State.WAITING
 
-        elif self.state == State.CALIBRATE:
-            # Starts calibration then moves to waiting
-            # await self.calibrate_service_client.call_async(request=Empty.Request())
-            self.state = State.LETTER
-
-        # elif self.state == State.APPROACHING:
-        #     # Calls the service for the approach pose then moves to writing state
-        #     #TODO: use cartician move
-        #     await self.movepose_service_client.call_async(self.current_mp_pose)
-        #     self.state = State.WRITING
-
         elif self.state == State.LETTER:
             if self.shape_list:
-                # moves to the approaching state if there are still things to be written
+                # Creates the correct data type for the next shape
                 await self.letter_writer(self.shape_list[0])
 
-                # self.state = State.WAITING
             else:
                 request4 = Cartesian.Request()
-                request4.poses = [Pose(position=Point(x=0.0, y=-0.3, z=0.3), orientation=Quaternion(
-                    x=0.7117299678289105, y=-0.5285053338340909, z=0.268057323473255, w=0.37718408812611504))]
+                request4.poses = [Pose(position=Point(x=0.0, y=-0.3, z=0.3),
+                                       orientation=Quaternion(x=0.71172996782,
+                                                              y=-0.5285053338,
+                                                              z=0.26805732347,
+                                                              w=0.3771840881))]
                 request4.velocity = 0.1
                 request4.replan = False
                 request4.use_force_control = [False]
-                await self.cartesian_mp_service_client.call_async(request4)
-                # Turns on the OCR because the play has ended and returns to WAITING
+                await self.cartesian_mp_client.call_async(request4)
+
                 goal_js = MovePose.Request()
-                # goal_js.joint_names = ["panda_joint4", "panda_joint5", "panda_joint7"]
-                # goal_js.joint_positions = [-2.61799, -1.04173, 2.11185]
                 goal_js.target_pose.position = Point(
-                    x=0.545029890155533, y=0.05943234468738731, z=0.5893544164237723)
+                    x=0.545029890155533, y=0.0594323446873873, z=0.58935441642)
                 goal_js.target_pose.orientation = Quaternion(
-                    x=-0.48576480709767544, y=-0.5175973920275, z=-0.4696623291331898, w=0.5249216975367619)
+                    x=-0.48576480709767544, y=-0.5175973920275,
+                    z=-0.4696623291331898, w=0.5249216975367619)
                 goal_js.use_force_control = False
-                ##################### moving to the position####################
-                self.get_logger().info('before moved')
-                await self.movepose_service_client.call_async(goal_js)
+                await self.movepose_client.call_async(goal_js)
+                # Turns on the OCR when play has ended and returns to WAITING
                 self.ocr_pub.publish(Bool(data=True))
                 self.state = State.WAITING
 
         elif self.state == State.WAITING:
             pass
-            # waiting state for writing actions
-            # if self.kick_future:
-            # Turns on OCR when kickstart finishes and waits for hangman callback
-
-            # self.kick_future = None
-            # elif self.calibrate_future:
-            #     # Listens for a return value from calibration to switch to LETTER
-
-            #     self.calibrate_future = None
-            # elif self.board_future:
-            #     # Looks that board has returned values
-            #     # Assigns poses for approach and cartesian then moves to APPROACHING
-            #     self.current_mp_pose = self.board_future.initial_pose
-            #     self.current_traj_poses = self.board_future.pose_list
-            #     self.board_future = None
-            #     self.state = State.APPROACHING
-            # else:
-            # If nothing has returned from client call, WAITING passes
-
-        # elif self.state == State.WRITING:
-        #     # waiting state for the Franka to complete the mp and cartesian trajectories
-        #     # in 2 steps before it moves back to LETTER
-        #     if self.movepose_future:
-        #         #TODO: update for use_forece_control
-        #         self.cartesian_mp_service_client.call_async(self.current_traj_poses)
-        #         self.movepose_future = None
-        #     else:
-        #         pass
-            # Node will only leave this state once trajectory_status returns 'done'
 
 
 def main(args=None):
--- ocr/ocr/hangman.py
+++ ocr/ocr/hangman.py
@@ -1,19 +1,31 @@
+"""
+Plays the hangman game based on the OCR user input.
+
+Interfaces with the brain node and the OCR node to evaulate data.
+PUBLISHERS:
+  + /writer (LetterMsg) - The data sent to brain for a given play.
+SERVICES:
+    none
+PARAMETERS:
+    none
+"""
+
 from enum import Enum, auto
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import String, Float64MultiArray
-from matplotlib.textpath import TextToPath
-from matplotlib.font_manager import FontProperties
+from std_msgs.msg import String
 import urllib.request
 from gameplay_interfaces.msg import LetterMsg
 from random import randint
 
+
 class State(Enum):
     """Create the states of the node to determine what the timer
-    fcn should be doing (MOVING or STOPPED)"""
+    fcn should be doing (PLAYING, WAITING, OR GAME_OVER)"""
     PLAYING = auto(),
     GAME_OVER = auto(),
     WAITING = auto()
+
 
 class Hangman(Node):
     """Plays the game hangman with user input"""
@@ -27,42 +39,24 @@ class Hangman(Node):
         self.guesses_to_fail = 5
         self.current_wrong_guesses = 0
         self.guessed_letters = []
-        self.word_status = ['_','_','_','_','_','_']
+        self.word_status = ['_', '_', '_', '_', '_', '_']
         self.game_won = False
         self.user_guess = None
         self.Alphabet = {}
-        self.man_list = ['0','|','-','/','_']
+        self.man_list = ['0', '|', '-', '/', '_']
 
         # Create Timer
         self.timer = self.create_timer(0.01, self.timer_callback)
 
         # Create Subscribers
-        self.input = self.create_subscription(String, "/user_input", self.user_input_callback, qos_profile=10)
+        self.input = self.create_subscription(
+            String, "/user_input", self.user_input_callback, qos_profile=10)
 
         # Create Publisher
-        self.writer = self.create_publisher(Float64MultiArray, "/writer", qos_profile=10, callback_group=None)
+        self.writer = self.create_publisher(
+            LetterMsg, "/writer", qos_profile=10, callback_group=None)
 
-        self.create_letters()
         self.pick_words()
-
-    def create_letters(self):
-        """Create the dictionary of bubble letters"""
-
-        letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-        for i in range(0,len(letters)):
-            letter = letters[i]
-            fp = FontProperties(family="MS Gothic", style="normal")
-            verts, codes = TextToPath().get_text_path(fp, letters[i])
-            # print(type(path))
-            # print(verts)
-            xlist = []
-            ylist = []
-            for j in range(0,len(verts)-1):
-                if verts[j][0] > 0:
-                    xlist.append(verts[j][0])
-                    ylist.append(verts[j][1])
-            point_dict = {letter: {'xlist': xlist,'ylist':ylist}}
-            self.Alphabet.update(point_dict)
 
     def pick_words(self):
         """Randomly chooses a 6 letter word"""
@@ -72,15 +66,15 @@ class Hangman(Node):
         long_txt = response.read().decode()
         words = long_txt.splitlines()
         word_list = []
-        for i in range(0,len(words)):
+        for i in range(0, len(words)):
             if len(words[i]) == 6:
                 word_list.append(words[i])
 
-        self.word = word_list[randint(0,len(word_list))].upper()
+        self.word = word_list[randint(0, len(word_list))].upper()
 
     def evaulate_guess(self, guess):
         """Evaluates the guess from the user"""
-        
+
         upper_guess = guess.upper()
         letter_list = []
         position_list = []
@@ -131,42 +125,24 @@ class Hangman(Node):
         else:
             self.get_logger().info('invalid guess')
         # sends the list of things to be written to be packaged and published
-        self.send_letter(positions=position_list, mode=mode_list, letters=letter_list)
-
-    def prompt_user(self):
-        """User prompt for testing, This would get replaced by OCR pipeline client"""
-        user_guess = input("What letter/word is your guess?")
-        self.evaulate_guess(user_guess)
+        self.send_letter(positions=position_list,
+                         mode=mode_list, letters=letter_list)
 
     def show_progress(self):
         """Shows the game progress"""
-        self.get_logger().info(f"Guessed wrong letters: {self.guessed_letters}")
+        self.get_logger().info(
+            f"Guessed wrong letters: {self.guessed_letters}")
         self.get_logger().info(f"Word Status: {self.word_status}")
         self.get_logger().info(f"Wrong guesses: {self.current_wrong_guesses}")
-        self.draw_man()
 
     def send_letter(self, letters, positions, mode):
-        """Publishes the list of points that define the letter to the ros topic for the franka to read"""
+        """Publishes the list of points that define the letter to
+        the ros topic for the franka to read"""
         letter_to_send = LetterMsg()
         letter_to_send.positions = positions
         letter_to_send.letters = letters
         letter_to_send.mode = mode
         self.writer.publish(letter_to_send)
-
-    def draw_man(self):
-        """lil fella"""
-        match self.current_wrong_guesses:
-
-            case 1:
-                self.get_logger().info("O \n------")
-            case 2:
-                self.get_logger().info("O- \n------")
-            case 3:
-                self.get_logger().info("O-| \n------")
-            case 4:
-                self.get_logger().info("O-|- \n------")
-            case 5:
-                self.get_logger().info("O-|-<  you died \n------")
 
     def check_word(self):
         """Checks the guessed word"""
@@ -178,20 +154,7 @@ class Hangman(Node):
         else:
             return False
 
-    def play_game(self):
-        """plays game for now. Will be replaced with timer callback"""
-
-        while self.current_wrong_guesses < self.guesses_to_fail and self.game_won == False:
-            if self.check_word() == False:
-                self.prompt_user()
-                self.show_progress()
-
-        if self.check_word() == True:
-            self.get_logger().info("Game won. good job.")
-        else:
-            self.get_logger().info("Game lost, try again later.")
-
-    def user_input_callback(self, msg:String):
+    def user_input_callback(self, msg: String):
         """Callback for the user input subscriber (check prompt user)"""
         self.user_guess = msg.data
         self.get_logger().info(f"Message: {self.user_guess}")
@@ -202,35 +165,38 @@ class Hangman(Node):
         if self.state == State.PLAYING:
             self.evaulate_guess(self.user_guess)
             if self.current_wrong_guesses < self.guesses_to_fail:
-                if self.check_word() == False:
+                if self.check_word() is False:
                     # self.prompt_user()
                     self.show_progress()
                     self.state = State.WAITING
 
-                if self.check_word() == True:
+                if self.check_word() is True:
                     self.get_logger().info("Game won. good job.")
                     self.show_progress()
                     self.state = State.GAME_OVER
             else:
-                self.get_logger().info(f"Game lost, try again later. Your word was {self.word}")
+                self.get_logger().info(
+                    f"Game lost, try again later. Your word was {self.word}")
                 self.show_progress()
                 self.state = State.GAME_OVER
 
         if self.state == State.WAITING:
             # Check if the player has done something
-            if self.user_guess == None:
+            if self.user_guess is None:
                 pass
-            elif self.user_guess in self.guessed_letters or self.user_guess in self.word_status:
+            elif self.user_guess in self.guessed_letters \
+                    or self.user_guess in self.word_status:
                 pass
             else:
                 self.state = State.PLAYING
 
         if self.state == State.GAME_OVER:
             pass
-        
+
 # test = Hangman()
 
 # test.play_game()
+
 
 def main(args=None):
     """ The node's entry point """
@@ -238,6 +204,7 @@ def main(args=None):
     node = Hangman()
     rclpy.spin(node)
     rclpy.shutdown()
+
 
 if __name__ == "__main__":
     main()
